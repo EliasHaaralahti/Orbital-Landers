f538df739ff96e4bc32400bf933aee02bb0869d3
Elias Haaralahti <Haaralahti.Elias@gmail.com>
2017-06-04 16:54:20 +0300

    use p2 physics system, get basic input

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,8 +1,8 @@
-var game = null;
-
-game = new Phaser.Game(window.innerHeight, window.innerWidth, Phaser.AUTO);
-
 var GameState = {
+    init: function() {
+        this.game.physics.startSystem(Phaser.Physics.P2JS);
+        this.game.physics.p2.gravity.y = 30;
+    },
 
     preload: function () {
         this.load.image('background', 'graphics/temp_background.png');
@@ -16,13 +16,32 @@ var GameState = {
         this.spaceShip = this.game.add.sprite(this.game.world.centerX,
             this.game.world.centerY, 'spaceShip')
         this.spaceShip.anchor.setTo(0.5, 0.5);
-        this.spaceShip.scale.setTo(0.25);
+        this.spaceShip.scale.setTo(0.15);
+        this.physics.p2.enable(this.spaceShip);
     },
 
     update: function() {
-        this.spaceShip.angle += 0.5;
+        if(this.game.input.keyboard.isDown(Phaser.Keyboard.W) ||
+            this.game.input.keyboard.isDown(Phaser.Keyboard.O)) {
+                this.spaceShip.body.velocity.y -= 1.5;
+                // TODO: Instead of just settings y position
+                // Calculate direction vector and set x and y position
+                // according to it
+        }
+        
+        if(this.game.input.keyboard.isDown(Phaser.Keyboard.A) ||
+            this.game.input.keyboard.isDown(Phaser.Keyboard.K)) {
+                this.spaceShip.body.angle -= 0.5;
+        }
+
+        if(this.game.input.keyboard.isDown(Phaser.Keyboard.D) ||
+            this.game.input.keyboard.isDown(Phaser.Keyboard.L)) {
+                 this.spaceShip.body.angle += 0.5;
+        }
     }
 }
+
+var game = new Phaser.Game(window.innerHeight, window.innerWidth, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
 game.state.start('GameState')
