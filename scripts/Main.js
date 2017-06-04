fb2b5d69d42448947f179031ee3103dfbe782c2c
Elias Haaralahti <Haaralahti.Elias@gmail.com>
2017-06-05 02:18:42 +0300

    add collisions

--- scripts/Main.js
+++ scripts/Main.js
@@ -2,9 +2,17 @@ const gravity = 30;
 const thrust = 50;
 const rotateSpeed = 1;
 
+// https://phaser.io/examples/v2/category/p2-physics
+// TODO: Use impact events instead? Need to figure which player hit what
+
+// Idea for ground: https://phaser.io/examples/v2/box2d/car-on-terrain
+// On collision with ground, check distance to all bonus labels? if close enough
+// to one, apply score multiplier?
+
 var GameState = {
     init: function() {
         this.game.physics.startSystem(Phaser.Physics.P2JS);
+        this.game.physics.p2.setImpactEvents(true);
         this.game.physics.p2.gravity.y = gravity;
     },
 
@@ -20,20 +28,20 @@ var GameState = {
 
         this.player1 = this.game.add.sprite(this.game.world.centerX - 150,
             this.game.world.centerY, 'spaceShip')
-        this.player1.anchor.setTo(0.5, 0.5);
-        this.player1.scale.setTo(0.15);
+        setUpPlayer(this.player1);
         this.physics.p2.enable(this.player1);
+        this.player1.body.onBeginContact.add(onHit, this);
 
         this.player2 = this.game.add.sprite(this.game.world.centerX + 150,
             this.game.world.centerY, 'spaceShip')
-        this.player2.anchor.setTo(0.5, 0.5);
-        this.player2.scale.setTo(0.15);
+        setUpPlayer(this.player2);
         this.physics.p2.enable(this.player2);
+        this.player2.body.onBeginContact.add(onHit, this);
     },
 
     update: function() {
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.W)) {
-            vectorMove(this.player1, 90)
+            this.player1.body.thrust(thrust);
         }
         
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.A)) {
@@ -43,14 +51,13 @@ var GameState = {
         }
 
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.D)) {
-            console.log(this.player1.body.angle)
             if(this.player1.body.angle < 90) {
                 this.player1.body.angle += rotateSpeed;
             }
         }
 
          if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            vectorMove(this.player2, 90)
+            this.player2.body.thrust(thrust);
         }
         
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
@@ -67,21 +74,20 @@ var GameState = {
     }
 }
 
+function onHit(body1, body2, shape1 ,shape2, equation) {
+    if(body1) {
+        console.log(body2.sprite + " hit: " + body1.sprite.key)
+    } else {
+        console.log("hit a wall!");
+    }
+}
+
+function setUpPlayer(player) {
+    player.anchor.setTo(0.5, 0.5);
+    player.scale.setTo(0.15);
+}
+
 var game = new Phaser.Game(window.innerHeight, window.innerWidth, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
 game.state.start('GameState')
-
-function vectorMove(gameobject, offset=0) {
-    var rotation = gameobject.body.angle;
-    var direction = new Phaser.Point();
-    direction.x = Math.cos(this.game.math.degToRad(rotation-offset));
-    direction.y = Math.sin(this.game.math.degToRad(rotation-offset));
-
-    if(direction.getMagnitude() > 0) {
-        direction = direction.normalize()
-    }
-
-    gameobject.body.velocity.x = direction.x * thrust;
-    gameobject.body.velocity.y = direction.y * thrust;
-}
