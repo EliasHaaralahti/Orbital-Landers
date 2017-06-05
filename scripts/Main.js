fb00b75f709e4577ac907443a331c4c19c87c4b3
EliasHaaralahti <haaralahti.elias@gmail.com>
2017-06-05 20:32:08 +0300

    Change physicsSystem to arcade

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,5 +1,5 @@
 const gravity = 30;
-const thrust = 50;
+const thrust = 1.5;
 const rotateSpeed = 1;
 
 // https://phaser.io/examples/v2/category/p2-physics
@@ -9,11 +9,16 @@ const rotateSpeed = 1;
 // On collision with ground, check distance to all bonus labels? if close enough
 // to one, apply score multiplier?
 
+var GREEN =  0x00ff00;
+var RED =    0xff0000;
+var WHITE =  0xffffff;
+var YELLOW = 0xffff00;
+
+
 var GameState = {
     init: function() {
-        this.game.physics.startSystem(Phaser.Physics.P2JS);
-        this.game.physics.p2.setImpactEvents(true);
-        this.game.physics.p2.gravity.y = gravity;
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.physics.arcade.gravity.y = 50;
     },
 
     preload: function () {
@@ -23,68 +28,115 @@ var GameState = {
     },
 
     create: function() {
-        this.background = this.add.sprite(
-            this.world.centerX, this.world.centerY, 'background');
+      this.line = createLine(100, 100, 500, 200);
+      game.physics.arcade.enable(this.line, Phaser.Physics.ARCADE);
 
-        this.player1 = this.game.add.sprite(this.game.world.centerX - 150,
-            this.game.world.centerY, 'spaceShip')
-        setUpPlayer(this.player1);
-        this.physics.p2.enable(this.player1);
-        this.player1.body.onBeginContact.add(onHit, this);
+      this.background = this.add.sprite(
+          this.world.centerX, this.world.centerY, 'background');
 
-        this.player2 = this.game.add.sprite(this.game.world.centerX + 150,
-            this.game.world.centerY, 'spaceShip')
-        setUpPlayer(this.player2);
-        this.physics.p2.enable(this.player2);
-        this.player2.body.onBeginContact.add(onHit, this);
+      this.player1 = this.game.add.sprite(this.game.world.centerX - 150,
+          this.game.world.centerY, 'spaceShip')
+      setUpPlayer(this.player1);
+      game.physics.arcade.enable(this.player1);
+      this.player1.body.enable = true;
+      this.player1.body.drag.set(10);
+      this.player1.body.collideWorldBound = true;
+      this.player1.body.maxVelocity.set(200);
+
+      this.player1.onCollide = new Phaser.Signal();
+      this.player1.onCollide.add(onCollision, this);
+
+
+      this.player2 = this.game.add.sprite(this.game.world.centerX + 150,
+          this.game.world.centerY, 'spaceShip')
+      setUpPlayer(this.player2);
+      game.physics.arcade.enable(this.player2);
+      this.player2.body.enable = true;
+      this.player2.body.drag.set(10);
+      this.player2.body.collideWorldBound = true;
+      this.player2.body.maxVelocity.set(200);
+
+      this.player2.onCollide = new Phaser.Signal();
+      this.player2.onCollide.add(onCollision, this);
+
     },
 
     update: function() {
+      game.physics.arcade.collide(this.player1, this.line);
+      game.physics.arcade.collide(this.player1, this.player2);
+
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.W)) {
-            this.player1.body.thrust(thrust);
+          moveUp(this.player1, thrust, -90)
         }
-        
+
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.A)) {
-            if(this.player1.body.angle > -90) {
-                this.player1.body.angle -= rotateSpeed;
+            if(this.player1.angle > -90) {
+                this.player1.angle -= rotateSpeed;
             }
         }
 
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.D)) {
-            if(this.player1.body.angle < 90) {
-                this.player1.body.angle += rotateSpeed;
+            if(this.player1.angle < 90) {
+                this.player1.angle += rotateSpeed;
             }
         }
 
          if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            this.player2.body.thrust(thrust);
+            moveUp(this.player2, thrust, -90)
         }
-        
+
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-            if(this.player2.body.angle > -90) {
-                this.player2.body.angle -= rotateSpeed;
-            }
+          if(this.player2.angle > -90) {
+              this.player2.angle -= rotateSpeed;
+          }
         }
 
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            if(this.player2.body.angle < 90) {
-                this.player2.body.angle += rotateSpeed;
-            }
+          if(this.player2.angle < 90) {
+              this.player2.angle += rotateSpeed;
+          }
+        }
+
+        if(this.game.input.keyboard.isDown(Phaser.Keyboard.H)) {
+          console.log(this.player1.position.y)
         }
     }
 }
 
-function onHit(body1, body2, shape1 ,shape2, equation) {
-    if(body1) {
-        console.log(body2.sprite + " hit: " + body1.sprite.key)
-    } else {
-        console.log("hit a wall!");
+function onCollision(object, collider) {
+  console.loc(object);
+  console.log(collider);
+}
+
+function moveUp(gameobject, thrust, offset) {
+   var rotation = gameobject.angle;
+   var direction = new Phaser.Point();
+   direction.x = Math.cos(this.game.math.degToRad(rotation-offset));
+   direction.y = Math.sin(this.game.math.degToRad(rotation-offset));
+
+    if(direction.getMagnitude() > 0) {
+        direction = direction.normalize()
     }
+
+    gameobject.body.velocity.x += direction.x * -thrust;
+    gameobject.body.velocity.y += direction.y * -thrust;
 }
 
 function setUpPlayer(player) {
     player.anchor.setTo(0.5, 0.5);
     player.scale.setTo(0.15);
+    player.boundsPadding = 0;
+}
+
+function createLine(x1, y1, x2, y2) {
+  var line = game.add.graphics(0,0);
+  line.lineStyle(2, WHITE).moveTo(x1, y1).lineTo(x2, y2);
+  line.boundsPadding = 0;
+
+  var shapeSprite = game.add.sprite(0,0);
+  shapeSprite.addChild(line)
+
+  return shapeSprite;
 }
 
 var game = new Phaser.Game(window.innerHeight, window.innerWidth, Phaser.AUTO);
