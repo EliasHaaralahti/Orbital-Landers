a2fe8a4f4931894f8e996d5991ed6c1459fb2837
Elias Haaralahti <Haaralahti.Elias@gmail.com>
2017-06-06 21:27:32 +0300

    stats, collision improvement, game logic

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,5 +1,5 @@
 const gravity = 11;
-const thrust = 0.5;
+const thrust = 0.3;
 const rotateSpeed = 1;
 
 const GREEN =  0x00ff00;
@@ -7,7 +7,7 @@ const RED =    0xff0000;
 const WHITE =  0xffffff;
 const YELLOW = 0xffff00;
 
-var fontStyle = { font: "64px Arial", fill: RED, backgroundColor: YELLOW }
+var fontstyle_stats = { font: "64px Arial", fill: RED, backgroundColor: YELLOW }
 
 var labels = []
 
@@ -31,31 +31,10 @@ var GameState = {
     },
 
     create: function() {
-        /*
-        var line = this.game.add.graphics(0,0);
-        line.lineStyle(2, WHITE).moveTo(100, 100).lineTo(300, 200);
-        // line.boundsPadding = 0;
-
-        shapeSprite = this.game.add.sprite(0,0, 'spaceShip');
-        shapeSprite.name = "ShapeSprite";
-        shapeSprite.addChild(line)
-        // game.physics.arcade.enable(shapeSprite);
-        game.physics.enable(shapeSprite, Phaser.Physics.ARCADE)
-        // shapeSprite.body.enabled = true;
-        // shapeSprite.body.allowGravity = false
-        */
-        var text = game.add.text(-150, -150, "testiiiiiiiiiiiiiiiiing", fontStyle);
-        text.anchor.set(0.5);
-        text.width = 50;
-        text.height = 50;
-
       this.background = this.add.sprite(
           this.world.centerX, this.world.centerY, 'background');
         this.background.scale.setTo(1.5)
         this.background.anchor.set(0.5)
-
-        var text2 = game.add.text(150, 150, "testtteagg");
-        text2.anchor.set(0.5);
 
     this.ground = this.add.sprite(
         this.world.centerX-400, this.world.centerY+350, 'ground');
@@ -73,7 +52,7 @@ var GameState = {
       player1.name = "Player1"
       game.physics.arcade.enable(player1);
       player1.body.collideWorldBound = true;
-      player1.body.maxVelocity.set(25);
+      player1.body.maxVelocity.set(50);
 
 
       player2 = this.game.add.sprite(this.game.world.centerX + 150,
@@ -82,12 +61,35 @@ var GameState = {
       player2.name = "Player2"
       game.physics.arcade.enable(player2);
       player2.body.collideWorldBound = true;
-      player2.body.maxVelocity.set(25);
+      player2.body.maxVelocity.set(50);
+
+        var style_stats = { font: "15px Arial", fill: "#ffffff" , align: "center" };
+        var style_announce = { font: "60px Arial", fill: "#ffffff", align: "center" };
+
+        this.name1 = game.add.text(20, 10, "Player1", style_stats);
+        this.xVel1 = game.add.text(20, 30, "Player1 x velocity", style_stats);
+        this.yVel1 = game.add.text(20, 50, "Player1 y velocity", style_stats);
+        this.rot1 = game.add.text(20, 70, "Player1 rotation", style_stats);
+
+        this.name2 = game.add.text(1750, 10, "Player2", style_stats);
+        this.xVel2 = game.add.text(1750, 30, "Player1 x velocity", style_stats);
+        this.yVel2 = game.add.text(1750, 50, "Player1 y velocity", style_stats);
+        this.rot2 = game.add.text(1750, 70, "Player1 rotation", style_stats);
+
+        this.gameOverText = game.add.text(this.game.world.centerX, this.game.world.centerY, "", style_announce);
+        this.gameOverText.anchor.set(0.5);
     },
 
     update: function() {
+        this.xVel1.text = "Horizontal: " + player1.body.velocity.x;
+        this.yVel1.text = "Vertical: " + player1.body.velocity.y;
+        this.rot1.text = "Angle: " + player1.angle;
+
+        this.xVel2.text = "Horizontal: " + player2.body.velocity.x;
+        this.yVel2.text = "Vertical: " + player2.body.velocity.y;
+        this.rot2.text = "Angle: " + player2.angle;
+
         game.physics.arcade.collide(player1, player2, onCollision, null, this);
-        // game.physics.arcade.collide(player1, shapeSprite, onCollision, null, this);
         game.physics.arcade.collide(player1, this.ground, onCollision, null, this);
         game.physics.arcade.collide(player2, this.ground, onCollision, null, this);
 
@@ -132,11 +134,13 @@ var GameState = {
 function onCollision(object, collider) {
     if(collider.name == "Ground") {
         object.body.velocity.x = 0;
+        if(object.angle < -15 || object.angle > 15) {
+            console.log(object.name + " goes BOOM!")
+            return;
+        }
         if(!gameOver) {
-            gameOver = true;
-            console.log("Collision between: " + object.name + " and " + collider.name)
-            console.log("timer: " + game.time.totalElapsedSeconds())
-            console.log("Winner: " + object.name)
+            gameOver = true;        
+            this.gameOverText.text =  "Game Over!\nWinner: " + object.name + "!\nTime: " + game.time.totalElapsedSeconds();
         }   
     }
 }
